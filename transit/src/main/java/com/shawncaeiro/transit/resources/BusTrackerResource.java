1e26478b0fae554ea9d77f7c53f85f81f787498d
Shawn Caeiro <shawncaeiro@gmail.com>
2017-01-05 22:14:30 -0600

    Begin second endpoint, just stop id

--- transit/src/main/java/com/shawncaeiro/transit/resources/BusTrackerResource.java
+++ transit/src/main/java/com/shawncaeiro/transit/resources/BusTrackerResource.java
@@ -27,7 +27,9 @@ import org.apache.http.impl.client.HttpClients;
 public class BusTrackerResource {
 
 	String apiKey;
-	String urlPattern = "http://www.ctabustracker.com/bustime/api/v2/getpredictions?key=%s&rt=%s&stpid=%s&format=json";
+	String urlPatternStopAndRoute = "http://www.ctabustracker.com/bustime/api/v2/getpredictions?key=%s&rt=%s&stpid=%s&format=json";
+	String urlPatternStopOnly = "http://www.ctabustracker.com/bustime/api/v2/getpredictions?key=%s&stpid=%s&format=json";
+
 
 	public BusTrackerResource(String apiKey) {
 		this.apiKey = apiKey;
@@ -38,7 +40,7 @@ public class BusTrackerResource {
     public Response allEvents(@PathParam("routeId") String routeId,
 							@PathParam("stopId") String stopId) {
 
-		String url = String.format(urlPattern,
+		String url = String.format(urlPatternStopAndRoute,
 				apiKey, routeId, stopId);
 
     	CloseableHttpClient httpclient = HttpClients.createDefault();
@@ -73,7 +75,48 @@ public class BusTrackerResource {
 
 		return Response.ok().entity(res).build();
     }
-    
+
+	@GET
+	@Path("stop/{stopId}")
+	public Response allEvents(@PathParam("stopId") String stopId) {
+
+		String url = String.format(urlPatternStopOnly,
+				apiKey, stopId);
+
+		CloseableHttpClient httpclient = HttpClients.createDefault();
+
+		CloseableHttpResponse response = null;
+		Prediction prediction = null;
+		try {
+			HttpGet httpGet = new HttpGet(url);
+			response = httpclient.execute(httpGet);
+			System.out.println(response.getStatusLine());
+			ObjectMapper objectMapper = new ObjectMapper();
+			prediction = objectMapper.readValue(response.getEntity().getContent(), Prediction.class);
+
+		} catch (ParseException | IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				response.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		List<PredictionResponse> arrivals = new ArrayList<>();
+		List<String> arrivalTimes = new ArrayList<>();
+		for (Prediction.BusTimeResponse.Prd prd : prediction.busTimeResponse.prd) {
+			arrivalTimes.add(prd.prdctdn);
+		}
+		PredictionResponse res = new PredictionResponse(
+				prediction.busTimeResponse.prd[0].rtdd,
+				prediction.busTimeResponse.prd[0].stpnm,
+				arrivalTimes);
+
+		return Response.ok().entity(res).build();
+	}
+
     public static void main(final String[] args) throws JSONException {
     	BusTrackerResource bus = new BusTrackerResource("APIKEY");
 		bus.allEvents("77", "4996");
